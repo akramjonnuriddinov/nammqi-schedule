68e0133452658aac2dd5c171c9b15f0a488f0028
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-11-26 14:03:41 +0500

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   scrollBehavior() {
     return { top: 0, behavior: 'smooth' }
   },
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
